43004468e377311bf2057eb5fbaf695c0f09d084
pengdiandian001 <379125600@qq.com>
2013-10-31 00:16:19 +0800

    PaymentInformation+FinalConfirmation

---
+++ MemberOnly/PaymentInformation.aspx
@@ -0,0 +1,201 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AsiaWebShopSite.master" AutoEventWireup="true" CodeFile="PaymentInformation.aspx.cs" Inherits="MemberOnly_PaymentInformation" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+    <style type="text/css">
+        .style2
+        {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: large;
+            text-decoration: underline;
+            color: #0000FF;
+        }
+        .style3
+        {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: medium;
+            color: #0000FF;
+        }
+        .style4
+        {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: medium;
+            color: #0000FF;
+            text-decoration: underline;
+        }
+        .style5
+        {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: small;
+            color: #0000FF;
+        }
+        .style6
+        {
+            width: 100%;
+        }
+        .style7
+        {
+            text-decoration: underline;
+            color: #0000FF;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <h1 class="style2">
+        <strong style="font-weight: 700; color: #0000CC">Specify Payment Information</strong></h1>
+    <p class="style3">
+        Dear
+        <asp:Label ID="UserName" runat="server"></asp:Label>
+        , Asia Web Shop only accept major credit cards:</p>
+    <p class="style4">
+        Add a card</p>
+    <p class="style5">
+        Enter your card information:</p>
+    <table class="style6">
+        <tr>
+            <td>
+                Cardholder Name:
+                <asp:TextBox ID="CardholderName" runat="server" 
+                    ValidationGroup="RegisterUserValidationGroup"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvCardHolderName" runat="server" 
+                ControlToValidate="CardholderName" Display="Dynamic" EnableClientScript="False" 
+                ErrorMessage="Cardholder Name is required." ForeColor="Red" 
+                ValidationGroup="RegisterUserValidationGroup">*</asp:RequiredFieldValidator>
+            </td>
+            <td>
+                Card Type:
+            <asp:DropDownList ID="CardTypeDropDownList" runat="server" 
+                Width="250px" Height="22px" ValidationGroup="RegisterUserValidationGroup">
+                <asp:ListItem>-- Select credit card --</asp:ListItem>
+                <asp:ListItem>American Express</asp:ListItem>
+                <asp:ListItem>Diners Club</asp:ListItem>
+                <asp:ListItem>Discover</asp:ListItem>
+                <asp:ListItem>MasterCard</asp:ListItem>
+                <asp:ListItem>Visa</asp:ListItem>
+            </asp:DropDownList>
+            <asp:RequiredFieldValidator ID="rfvCardTypeDropDownList" runat="server" 
+                ControlToValidate="CardTypeDropDownList" Display="Dynamic" 
+                EnableClientScript="False" ErrorMessage="Please select a credit card." 
+                ForeColor="Red" InitialValue="-- Select credit card --" 
+                ValidationGroup="RegisterUserValidationGroup">*</asp:RequiredFieldValidator>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Card Number:
+                <asp:TextBox ID="CardNumber" runat="server" 
+                    ValidationGroup="RegisterUserValidationGroup"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvCardNumber" runat="server" 
+                ControlToValidate="CardNumber" Display="Dynamic" EnableClientScript="False" 
+                ErrorMessage="Card Number is required." ForeColor="Red" 
+                ValidationGroup="RegisterUserValidationGroup">*</asp:RequiredFieldValidator>
+            <asp:RegularExpressionValidator ID="revCardNumber" runat="server" 
+                ControlToValidate="CardNumber" Display="Dynamic" EnableClientScript="False" 
+                ErrorMessage="Card Number must be numeric and between 14 and 16 digits." 
+                ForeColor="Red" ValidationExpression="^\d{14,16}$" 
+                ValidationGroup="RegisterUserValidationGroup">*</asp:RegularExpressionValidator>
+            </td>
+            <td>
+                Expiry Date:
+                        <asp:DropDownList ID="MonthDropDownList" runat="server" 
+                            Height="22px" Width="110px" CausesValidation="True" 
+                            ValidationGroup="RegisterUserValidationGroup">
+                            <asp:ListItem Value="00">Month</asp:ListItem>
+                            <asp:ListItem>01</asp:ListItem>
+                            <asp:ListItem>02</asp:ListItem>
+                            <asp:ListItem>03</asp:ListItem>
+                            <asp:ListItem Value="04"></asp:ListItem>
+                            <asp:ListItem Value="05"></asp:ListItem>
+                            <asp:ListItem Value="06"></asp:ListItem>
+                            <asp:ListItem Value="07"></asp:ListItem>
+                            <asp:ListItem Value="08"></asp:ListItem>
+                            <asp:ListItem Value="09"></asp:ListItem>
+                            <asp:ListItem Value="10"></asp:ListItem>
+                            <asp:ListItem Value="11"></asp:ListItem>
+                            <asp:ListItem Value="12"></asp:ListItem>
+                        </asp:DropDownList>
+                        <asp:RequiredFieldValidator ID="rfvMonth" runat="server" 
+                            ControlToValidate="MonthDropDownList" Display="Dynamic" 
+                            EnableClientScript="False" ErrorMessage="Please select a month." 
+                            ForeColor="Red" InitialValue="00" 
+                            ValidationGroup="RegisterUserValidationGroup">*</asp:RequiredFieldValidator>
+                    &nbsp;/ 
+                        <asp:DropDownList ID="YearDropDownList" runat="server" 
+                            Height="22px" Width="110px" 
+                    ValidationGroup="RegisterUserValidationGroup">
+                        </asp:DropDownList>
+                        <asp:RequiredFieldValidator ID="rfvYear" runat="server" 
+                            ControlToValidate="YearDropDownList" Display="Dynamic" 
+                            EnableClientScript="False" ErrorMessage="Please select a year." ForeColor="Red" 
+                            InitialValue="0" ValidationGroup="RegisterUserValidationGroup">*</asp:RequiredFieldValidator>
+                        <asp:CustomValidator ID="cvExpiryDate" runat="server" 
+                            ControlToValidate="MonthDropDownList" Display="Dynamic" 
+                            EnableClientScript="False" ErrorMessage="Credit card is expired." 
+                            ForeColor="Red" onservervalidate="cvExpiryDate_ServerValidate" 
+                            ValidationGroup="RegisterUserValidationGroup">*</asp:CustomValidator>
+                    </td>
+        </tr>
+    </table>
+    <p>
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btAddYourCard" runat="server" onclick="btAddYourCard_Click" 
+            Text="Add Your Card" ValidationGroup="RegisterUserValidationGroup" />
+    </p>
+    <p class="style7">
+        Or Select From Your Credit Card List
+    </p>
+    <p>
+        <asp:GridView ID="gvCreditCard" runat="server" AutoGenerateColumns="False" 
+            CellPadding="4" DataKeyNames="number" DataSourceID="SqlDataSource1" 
+            ForeColor="#333333" GridLines="None"> 
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:CheckBox ID="CheckBox1" runat="server" 
+                            oncheckedchanged="CheckBox1_CheckedChanged" Text=" " />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="cardHolderName" HeaderText="cardHolderName" 
+                    SortExpression="cardHolderName" />
+                <asp:BoundField DataField="type" HeaderText="type" SortExpression="type" />
+                <asp:BoundField DataField="number" HeaderText="number" ReadOnly="True" 
+                    SortExpression="number" />
+                <asp:BoundField DataField="expiryMonth" HeaderText="expiryMonth" 
+                    SortExpression="expiryMonth" />
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:AsiaWebShopDBConnectionString %>" 
+            SelectCommand="SELECT [cardHolderName], [type], [number], [expiryMonth], [expiryYear] FROM [CreditCard] WHERE ([userName] = @userName)">
+            <SelectParameters>
+                <asp:ControlParameter ControlID="UserName" Name="userName" PropertyName="Text" 
+                    Type="String" />
+            </SelectParameters>
+        </asp:SqlDataSource>
+    </p>
+    <p>
+        <asp:Label ID="SelectOneCardOnlyMessage" runat="server" ForeColor="Red"></asp:Label>
+&nbsp;</p>
+    <asp:ValidationSummary ID="ValidationSummary1" runat="server" 
+        EnableClientScript="False" ForeColor="Red" HeaderText="The following errors:" 
+        ValidationGroup="RegisterUserValidationGroup" />
+    <p>
+    </p>
+    <p class="style7">
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btContinue" runat="server" onclick="btContinue_Click" 
+            Text="Continue" CausesValidation="False" />
+    </p>
+</asp:Content>
+
---
+++ MemberOnly/PaymentInformation.aspx.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class MemberOnly_PaymentInformation : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            gvCreditCard.DataBind();
+            // Populate the YearDropDownList from current year to plus 10 years.
+            YearDropDownList.Items.Add(new ListItem("Year", "0"));
+            for (int year = DateTime.Now.Year; year <= DateTime.Now.Year + 10; year++)
+            {
+                YearDropDownList.Items.Add(year.ToString());
+            }
+
+            string connectionString = "AsiaWebShopDBConnectionString";
+            string userName = User.Identity.Name;
+            GetMemberData(connectionString, userName);
+
+            // Hide and clear the edit result message.
+            SelectOneCardOnlyMessage.Visible = false;
+            SelectOneCardOnlyMessage.Text = "";
+    
+            //GetMemberCreditCard(connectionString, userName);
+
+        }
+    }
+
+    /*private void GetMemberCreditCard(string connectionString, string userName)
+    {
+        //System.Diagnostics.Debug.WriteLine("Enter GetMemberCreditCard");
+        // Define the SELECT query to get the member's credit card.
+        string query = "SELECT [number], [type], [cardHolderName], [expiryMonth], [expiryYear] FROM [CreditCard] WHERE ([username] =N'" + userName + "')";
+
+        // Create the connection and the SQL command.
+        using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings[connectionString].ConnectionString))
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            // Open the connection.
+            command.Connection.Open();
+            // Execute the SELECT query and place the result in a DataReader.
+            SqlDataReader reader = command.ExecuteReader();
+            // Check if a result was returned.
+            if (reader.HasRows)
+            {
+                // Iterate through the table to get the retrieved values.
+                while (reader.Read())
+                {
+                    // Assign the data values to the web form labels.
+                    CardHolderName.Text = reader["cardHolderName"].ToString().Trim();
+                    CardNumber.Text = reader["number"].ToString().Trim();
+                    CardTypeDropDownList.Text = reader["type"].ToString().Trim();
+                    MonthDropDownList.Text = reader["expiryMonth"].ToString().Trim();
+
+                    // System.Diagnostics.Debug.WriteLine("GetMemberCreditCard_MonthDropDownList.SelectedItem.Value:");
+                    // System.Diagnostics.Debug.WriteLine(MonthDropDownList.SelectedItem.Value);
+                    YearDropDownList.Text = reader["expiryYear"].ToString().Trim();
+                }
+            }
+
+            // Close the connection and the DataReader.
+            command.Connection.Close();
+            reader.Close();
+        }
+        //System.Diagnostics.Debug.WriteLine("Exit GetMemberCreditCard");
+    }*/
+
+    private void GetMemberData(string connectionString, string userName)
+    {
+        // Define the SELECT query to get the member's personal data.
+        string query = "SELECT [userName] FROM [Member] WHERE ([username] =N'" + userName + "')";
+
+        // Create the connection and the SQL command.
+        using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings[connectionString].ConnectionString))
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            // Open the connection.
+            command.Connection.Open();
+            // Execute the SELECT query and place the result in a DataReader.
+            SqlDataReader reader = command.ExecuteReader();
+            // Check if a result was returned.
+            if (reader.HasRows)
+            {
+                // Iterate through the table to get the retrieved values.
+                while (reader.Read())
+                {
+                    // Assign the data values to the web form label and textboxes.
+                    UserName.Text = reader["userName"].ToString().Trim();
+                }
+            }
+
+            // Close the connection and the DataReader.
+            command.Connection.Close();
+            reader.Close();
+        }
+    }
+
+    protected void AddCreditCard(string connectionString, string userName, string number, string type, string cardHolderName, string expiryMonth, string expiryYear)
+    {
+        // Define the INSERT query with parameters.
+        string query = "INSERT INTO [CreditCard]([userName], [number], [type], [cardHolderName], [expiryMonth], [expiryYear])" +
+                       "VALUES (@Username, @Number, @Type, @CardHolderName, @ExpiryMonth, @ExpiryYear)";
+        // Create the connection and the SQL command.
+        using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings[connectionString].ConnectionString))
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            // Define the UPDATE query parameters and their values.
+            command.Parameters.AddWithValue("@Username", userName);
+            command.Parameters.AddWithValue("@Number", number);
+            command.Parameters.AddWithValue("@Type", type);
+            command.Parameters.AddWithValue("@CardHolderName", cardHolderName);
+            command.Parameters.AddWithValue("@ExpiryMonth", expiryMonth);
+
+            //System.Diagnostics.Debug.WriteLine("UpdateCreditCard_MonthDropDownList.SelectedItem.Value:");
+            //System.Diagnostics.Debug.WriteLine(MonthDropDownList.SelectedItem.Value);
+
+            command.Parameters.AddWithValue("@ExpiryYear", expiryYear);
+
+            // Open the connection, execute the INSERT query and close the connection.
+            command.Connection.Open();
+            command.ExecuteNonQuery();
+            command.Connection.Close();
+        }
+    }
+
+    protected void updateCreditCardInOrderRecord(string connectionString, string userName, string creditCardNumber)
+    {
+        // Define the INSERT query with parameters.
+        string query = "INSERT INTO [OrderRecord]([userName], [creditCardNumber])" +
+                       "VALUES (@Username, @CreditCardNumber)";
+        // Create the connection and the SQL command.
+        using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings[connectionString].ConnectionString))
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            // Define the UPDATE query parameters and their values.
+            command.Parameters.AddWithValue("@Username", userName);
+            command.Parameters.AddWithValue("@CreditCardNumber", creditCardNumber);
+            /*command.Parameters.AddWithValue("@Type", type);
+            command.Parameters.AddWithValue("@CardHolderName", cardHolderName);
+            command.Parameters.AddWithValue("@ExpiryMonth", expiryMonth); */
+
+            //System.Diagnostics.Debug.WriteLine("UpdateCreditCard_MonthDropDownList.SelectedItem.Value:");
+            //System.Diagnostics.Debug.WriteLine(MonthDropDownList.SelectedItem.Value);
+
+            //command.Parameters.AddWithValue("@ExpiryYear", expiryYear);
+
+            // Open the connection, execute the INSERT query and close the connection.
+            command.Connection.Open();
+            command.ExecuteNonQuery();
+            command.Connection.Close();
+        }
+    }
+
+    protected void cvExpiryDate_ServerValidate(object source, ServerValidateEventArgs args)
+    {
+        if ((MonthDropDownList.SelectedValue.Trim() != "00") & (YearDropDownList.SelectedValue.Trim() != "0"))
+        {
+            Int16 month = Convert.ToInt16(MonthDropDownList.SelectedValue.Trim());
+            Int16 year = Convert.ToInt16(YearDropDownList.SelectedValue.Trim());
+            if ((DateTime.Now.Month > month) & (DateTime.Now.Year >= year))
+            {
+                args.IsValid = false;
+            }
+        }
+    }
+    protected void btAddYourCard_Click(object sender, EventArgs e)
+    {
+        Page.Validate("RegisterUserValidationGroup");
+        if (Page.IsValid)
+        {
+            string connectionString = "AsiaWebShopDBConnectionString";
+            string userName = User.Identity.Name;
+
+         
+
+            // After the information is added, add the credit card data in the credit card database.
+            AddCreditCard(connectionString,
+                userName.Trim(),
+                CardNumber.Text.Trim(),
+                CardTypeDropDownList.SelectedItem.Text.Trim(),
+                CardholderName.Text.Trim(),
+                MonthDropDownList.SelectedItem.Text.Trim(),
+                YearDropDownList.SelectedItem.Text.Trim());
+            // After the information is added, add the credit card data in the order record database.
+            updateCreditCardInOrderRecord(connectionString,
+               userName.Trim(),
+               CardNumber.Text.Trim());
+
+            FormsAuthentication.SetAuthCookie(userName.Trim(), false /* createPersistentCookie */);
+
+            string continueUrl = "~/MemberOnly/FinalConfirmation.aspx";
+            if (String.IsNullOrEmpty(continueUrl))
+            {
+                continueUrl = "~/";
+            }
+            Response.Redirect(continueUrl, false);
+        }
+    }
+
+
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+        int j = 0;
+        foreach (GridViewRow row in this.gvCreditCard.Rows)
+        {
+            Control ctrl = row.FindControl("CheckBox1");
+            if ((ctrl as CheckBox).Checked)
+            {
+                j++;
+            }
+            if (j > 1)
+            {
+                SelectOneCardOnlyMessage.Text = "Please select one credit card only." + SelectOneCardOnlyMessage.Text;
+                SelectOneCardOnlyMessage.Visible = true;
+            }
+                
+
+        }
+    
+    }
+    protected void btContinue_Click(object sender, EventArgs e)
+    {
+            string connectionString = "AsiaWebShopDBConnectionString";
+            string userName = User.Identity.Name;
+            string cardNumberSelected = "";
+            foreach (GridViewRow row in this.gvCreditCard.Rows)
+            {
+                Control ctrl = row.FindControl("CheckBox1");
+                if ((ctrl as CheckBox).Checked)
+                {
+                    TableCellCollection cell = row.Cells;
+                    cardNumberSelected = cell[3].Text.Trim();
+                    break;
+                }
+            }
+            // After the information is added, add the credit card data in the order record database.
+            updateCreditCardInOrderRecord(connectionString,
+            userName.Trim(),
+            cardNumberSelected);
+
+            FormsAuthentication.SetAuthCookie(userName.Trim(), false /* createPersistentCookie */);
+
+            string continueUrl = "~/MemberOnly/FinalConfirmation.aspx";
+            if (String.IsNullOrEmpty(continueUrl))
+            {
+                continueUrl = "~/";
+            }
+            Response.Redirect(continueUrl, false);
+    }
+
+}
